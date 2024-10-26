94b5f14329039d4035dc58eb7420d0b4eb98b5dc
Kim min-jae <mjkim11084@gmail.com>
2024-10-26 22:46:55 +0900

    🍻 Write a landing page code

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,42 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+'use client'
+
+import Link from "next/link";
+import styled from "styled-components";
+
+const Header = styled.header`
+  display: flex;
+  background-color: #a78bfa;
+  width:100rem;
+  height: 5rem;
+  align-items: center;
+`
+
+const HeaderWrapper = styled.div`
+  margin-left: 1rem;
+`
+
+const customLogo = {
+  fontSize: "2rem",
+  fontWeight: 550,
+  textDecoration: "none",
+  marginRight: "1rem"
+}
+
+const customLink = {
+  fontSize: "1.3rem",
+  fontWeight: 500,
+  textDecoration: "none",
+}
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+    <div>
+      <Header>
+        <HeaderWrapper>
+          <Link href="#" style={customLogo}>WordWise</Link>
+          <Link href="/designs" style={customLink}>DESIGNS</Link>
+        </HeaderWrapper>
+      </Header>
     </div>
   );
 }
